1d2bd18ed2db5c6a1caddd2489a6ade70544ab86
JMARyA <jmarya@hydrar.de>
2026-05-02 03:02:59 +0200

    ci: split into ci and publish moira pipelines
    Replace manual-only .moira/ci.yaml with two moiraPipelines in flake.nix:
    - ci: runs cargo check + test on push to main and PRs
    - publish: builds dockerImage via needs_flake, pushes to git.hydrar.de with skopeo
    - Add .moira/envs/publish.nix with skopeo for the push step

--- flake.nix
+++ flake.nix
@@ -8,8 +8,16 @@
     crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, crane }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      crane,
+    }:
+    (flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
@@ -24,26 +32,34 @@
           inherit src;
           strictDeps = true;
           nativeBuildInputs = [ pkgs.pkg-config ];
-          buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            pkgs.apple-sdk
-            pkgs.libiconv
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
-            pkgs.openssl
-          ];
+          buildInputs =
+            pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              pkgs.apple-sdk
+              pkgs.libiconv
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+              pkgs.openssl
+            ];
         };
 
         # Build deps-only crate for better layer caching
         cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-        sh3rine = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
-        });
+        sh3rine = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+          }
+        );
 
         # Minimal container image
         dockerImage = pkgs.dockerTools.buildLayeredImage {
           name = "sh3rine";
           tag = "latest";
-          contents = [ sh3rine pkgs.cacert ];
+          contents = [
+            sh3rine
+            pkgs.cacert
+          ];
           fakeRootCommands = ''
             mkdir -p etc/ssl
             ln -s ${pkgs.cacert}/etc/ssl/certs etc/ssl/certs
@@ -56,7 +72,9 @@
               "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
               "NIX_SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
             ];
-            ExposedPorts = { "8080/tcp" = {}; };
+            ExposedPorts = {
+              "8080/tcp" = { };
+            };
           };
         };
       in
@@ -69,5 +87,58 @@
         devShells.default = craneLib.devShell {
           packages = [ pkgs.rust-analyzer ];
         };
-      });
+      }
+    ))
+    // {
+      moiraPipelines = {
+        ci = {
+          trigger = {
+            on_push.branches = [ "main" ];
+            on_pr.target = [ "main" ];
+          };
+          sandbox_defaults.network = false;
+          vars = {
+            RUST_BACKTRACE = "1";
+            CARGO_TERM_COLOR = "always";
+          };
+          steps = [
+            {
+              name = "check";
+              env = ./.moira/envs/rust.nix;
+              run = "cargo check --workspace";
+            }
+            {
+              name = "test";
+              env = ./.moira/envs/rust.nix;
+              run = "cargo test --workspace";
+              depends_on = [ "check" ];
+            }
+          ];
+        };
+        publish = {
+          trigger = {
+            on_push.branches = [ "main" ];
+            manual = true;
+          };
+          needs_flake = [ "packages.dockerImage" ];
+          env = ./.moira/envs/publish.nix;
+          steps = [
+            {
+              name = "push-image";
+              run = ''
+                skopeo copy \
+                  --dest-creds "$REGISTRY_USER:$REGISTRY_PASS" \
+                  docker-archive:''${{ flake.packages.dockerImage }} \
+                  docker://git.hydrar.de/jmarya/sh3rine:latest
+              '';
+              sandbox.network = true;
+              secrets = [
+                "REGISTRY_USER"
+                "REGISTRY_PASS"
+              ];
+            }
+          ];
+        };
+      };
+    };
 }
