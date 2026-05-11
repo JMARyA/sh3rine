d543cc0f4928bddc4248c64a77a2b12a549dcdfd
jmarya <jmarya@hydrar.de>
2026-05-11 10:32:17 +0000

    .moira/envs/publish.nix aktualisiert

--- .moira/envs/publish.nix
+++ .moira/envs/publish.nix
@@ -3,4 +3,17 @@
 }:
 pkgs.mkShell {
   buildInputs = [ pkgs.skopeo ];
+
+shellHook = ''
+  cat > .containers-policy.json <<EOF
+  {
+    "default": [
+      { "type": "insecureAcceptAnything" }
+    ]
+  }
+  EOF
+
+  export CONTAINERS_POLICY_JSON=$PWD/.containers-policy.json
+'';
+
 }
